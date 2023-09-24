9e94b3a8e6148b7b11abe8754acd07d1545e8f8d
Paul Kang <paul@paul-kang.com>
2023-09-24 19:25:58 +0800

    wip

--- app/api/backup/route.ts
+++ app/api/backup/route.ts
@@ -4,11 +4,28 @@ import os from 'os';
 import fs from 'fs';
 import axios from 'axios';
 
+var userFileList: string[] = [];
+var measurementsFileList: string[] =[];
+
+function getFileLists() {
+  userFileList = [];
+  measurementsFileList =[];
+
+  fs.readdirSync("data").forEach(file => {
+    if (file.includes("users")) userFileList.push(file);
+    if (file.includes("measurements")) measurementsFileList.push(file);
+  })
+  
+}
+
 export async function GET(request: NextRequest) {
   console.log("Backup GET method");
 
   const nowTimeStamp = new Date().getTime();
   console.log("in backup api 14:", os.platform(), request.nextUrl.searchParams.get("cmd"), nowTimeStamp);
+
+  getFileLists();
+  console.log("in backup api 29:", userFileList, measurementsFileList);
 
   if (request.nextUrl.searchParams.get("cmd") == "backup") {
     try {
